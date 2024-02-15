428a5c4062f0c7e485d8f6c82de45ebfa6f04e19
Jannis Brake <78983365+Jannis15@users.noreply.github.com>
2024-02-15 21:50:16 +0100

    Beautify view

--- lib/widgets/bloc/processes_cubit.dart
+++ lib/widgets/bloc/processes_cubit.dart
@@ -9,26 +9,35 @@ abstract class ProcessesState {
   ProcessesState(this.repository);
 }
 
-class ProcessesStateLoading extends ProcessesState {
-  ProcessesStateLoading(super.repository);
+class ProcessesLoading extends ProcessesState {
+  ProcessesLoading(super.repository);
 }
-class ProcessesStateLoaded extends ProcessesState {
+
+class ProcessesLoaded extends ProcessesState {
   final List<ProcessModel> processes;
-  ProcessesStateLoaded(super.repository, {required this.processes});
+  final bool isFetchingInBackground;
+
+  ProcessesLoaded(super.repository, {required this.processes, required this.isFetchingInBackground});
 }
 
 class ProcessesCubit extends Cubit<ProcessesState> {
   ProcessesCubit(super.initialState);
 
-
   Future<void> initialize() async {
     final processes = await state.repository.getProcesses();
-    processes.sort((a, b) => b.duration.compareTo(a.duration),);
-    emit(ProcessesStateLoaded(state.repository, processes: processes));
+    processes.sort(
+      (a, b) => b.duration.compareTo(a.duration),
+    );
+    emit(ProcessesLoaded(state.repository, processes: processes, isFetchingInBackground: false));
   }
 
   Future<void> refresh() async {
-    emit(ProcessesStateLoading(state.repository));
-    await initialize();
+    if (state is ProcessesLoaded) {
+      final loadedState = state as ProcessesLoaded;
+      if (!loadedState.isFetchingInBackground) {
+        emit(ProcessesLoaded(loadedState.repository, processes: loadedState.processes, isFetchingInBackground: true));
+        await initialize();
+      }
+    }
   }
 }
--- lib/widgets/ui/home_view.dart
+++ lib/widgets/ui/home_view.dart
@@ -1,51 +1,133 @@
+import 'package:echse_frontend/config/constants.dart';
+import 'package:echse_frontend/config/global_service.dart';
 import 'package:echse_frontend/widgets/bloc/processes_cubit.dart';
 import 'package:echse_frontend/widgets/bloc/repository_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../data/prcoess_repository.dart';
 
-class HomeView extends StatelessWidget {
+const kIconSize = 64.0;
+
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
   Widget build(BuildContext context) {
+    Widget buildDefaultAppIcon() => const SizedBox(
+          width: kIconSize,
+          height: kIconSize,
+          child: Icon(
+            Icons.wysiwyg,
+            size: kIconSize - 16,
+          ),
+        );
+
     return MaterialApp(
+        navigatorKey: GlobalService.globalKey,
         theme: ThemeData.from(colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff325d9e), brightness: Brightness.light)),
         darkTheme: ThemeData.from(colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff325d9e), brightness: Brightness.dark)),
+        debugShowCheckedModeBanner: false,
         title: 'Demo',
         home: RepositoryProvider<RepositoryCubit>(
             create: (context) => RepositoryCubit(ProcessRepository()),
             child: BlocProvider<ProcessesCubit>(
-                create: (context) => ProcessesCubit(ProcessesStateLoading(context.read<RepositoryCubit>().state))..initialize(),
+                create: (context) => ProcessesCubit(ProcessesLoading(context.read<RepositoryCubit>().state))..initialize(),
                 child: BlocBuilder<ProcessesCubit, ProcessesState>(builder: (context, state) {
-                  if (state is ProcessesStateLoading) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (state is ProcessesStateLoaded) {
-                    return Scaffold(
-                      body: SafeArea(
-                          child: ListView.builder(
-                              itemCount: state.processes.length,
-                              itemBuilder: (context, index) {
-                                final process = state.processes[index];
-                                return Card(
-                                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      process.icon.isNotEmpty? Image.memory(process.icon,width: 32,height: 32,):const SizedBox(),
-                                      Text(process.name),
-                                      Text('Dauer (Minuten): ${process.duration}')
-                                    ],
-                                  ),
-                                );
-                              })),
-                    );
-                  } else {
-                    return const Center(
-                      child: Text("An unexpected error occured!"),
-                    );
-                  }
+                  return Scaffold(
+                    appBar: AppBar(
+                        title: Flex(
+                          direction: Axis.horizontal,
+                          children: [
+                            Icon(FontAwesomeIcons.windows, color: Theme.of(context).colorScheme.primary),
+                            const SizedBox(
+                              width: AppSizes.kBigGap,
+                            ),
+                            const Flexible(
+                              fit: FlexFit.loose,
+                              child: Text('Aktivitäten'),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          ElevatedButton.icon(
+                              label: const Text('Aktualisieren'),
+                              onPressed: state is ProcessesLoading || state is ProcessesLoaded && state.isFetchingInBackground
+                                  ? null
+                                  : () {
+                                      context.read<ProcessesCubit>().refresh();
+                                    },
+                              icon: const Icon(Icons.sync)),
+                          const SizedBox(
+                            width: AppSizes.kGap,
+                          )
+                        ]),
+                    body: state is ProcessesLoading
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : state is ProcessesLoaded
+                            ? SafeArea(
+                                child: ListView.separated(
+                                    controller: _scrollController,
+                                    separatorBuilder: (context, index) => const SizedBox(height: AppSizes.kGap),
+                                    itemCount: state.processes.length,
+                                    itemBuilder: (context, index) {
+                                      final process = state.processes[index];
+                                      int maxDuration =
+                                          state.processes.map((obj) => obj.duration).reduce((value, element) => value > element ? value : element);
+                                      return Flex(
+                                        direction: Axis.horizontal,
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Flexible(
+                                            fit: FlexFit.loose,
+                                            child: Container(
+                                              width: 800,
+                                              padding: const EdgeInsets.symmetric(horizontal: AppSizes.kBigGap, vertical: AppSizes.kGap),
+                                              child: Column(
+                                                children: [
+                                                  ListTile(
+                                                    titleAlignment: ListTileTitleAlignment.center,
+                                                    leading: process.icon.isNotEmpty
+                                                        ? Image.memory(
+                                                            process.icon,
+                                                            width: kIconSize,
+                                                            height: kIconSize,
+                                                            errorBuilder: (context, error, stackTrace) => buildDefaultAppIcon(),
+                                                          )
+                                                        : buildDefaultAppIcon(),
+                                                    title: Text(process.name),
+                                                    trailing: Text('${process.duration} Minute${process.duration == 1 ? '' : 'n'}',
+                                                        style: Theme.of(context).textTheme.bodyMedium),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: AppSizes.kBigGap,
+                                                  ),
+                                                  LinearProgressIndicator(
+                                                    borderRadius: BorderRadius.circular(AppSizes.kDefaultBorderRadius),
+                                                    value: process.duration / maxDuration,
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    }),
+                              )
+                            : const Center(
+                                child: Text('Ein unerwarteter Fehler ist aufgetreten!'),
+                              ),
+                  );
                 }))));
   }
 }
